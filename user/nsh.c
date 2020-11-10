3d77a257a8151d3b38e8912e5ac747f90207e462
nina <782272741@qq.com>
2020-11-11 00:00:57 +0800

    lab2-second-try

--- user/nsh.c
+++ user/nsh.c
@@ -12,7 +12,6 @@
 #define MAXARGS 10
 
 //参考自sh.c文件中的panic、fork1和getcmd函数
-
 void
 panic(char *s)
 {
@@ -42,6 +41,30 @@ getcmd(char *buf, int nbuf)
   return 0;
 }
 
+void clearcmd(char* cmd, char* argv[],int* argc){
+    for(int i=0; i<MAXARGS; i++){
+        argv[i] = args[i];
+    }
+    int i;
+    int j;
+    for(i = 0,j = 0; cmd[j] != '\n' && cmd[j] != '\0'; j++){
+        //跳过之前的空格
+        while(strchr(whitespace,cmd[j])){
+            j++;
+        }
+        //空格之后的命令存入argv
+        argv[i++] = cmd+j;
+        //寻找下一个空格
+        while(strchr(whitespace,cmd[j])){
+            j++;
+        }
+        cmd[j] = '\0';
+    }
+    argv[i] = 0;
+    *argc = i;
+}
+
+void runcmd()
 //参考自sh.c中的main函数
 int
 main(void)
@@ -58,8 +81,13 @@ main(void)
   }
   // Read and run input commands.
   while(getcmd(buf, sizeof(buf)) >= 0){
-    if(fork1() == 0)
-      runcmd(parsecmd(buf));
+    if(fork1() == 0){
+        char* argv[MAXARGS];
+        int argc = -1;
+        //重组参数格式
+        clearcmd(buf, argv, &argc);
+        runcmd(parsecmd(buf));
+    } 
     wait(0);
   }
   exit(0);
